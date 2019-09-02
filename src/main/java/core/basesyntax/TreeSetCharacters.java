3b3cf0a3c00d8debb419ac9a42a405ddb811b4d1
Taranarnik <pavelkurilyuk97@gmail.com>
2019-09-02 22:26:39 +0300

    added solution for tree set character homework

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +25,59 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+
+    private static final int SIZE_OF_OUTPUT_STRING = 5;
+
+    public String getUniqueCharacters(String fileName) throws FileNotFoundException {
+        File file = new File(fileName);
+        if (!file.exists()) {
+            throw new FileNotFoundException();
+        }
+        try (FileReader reader = new FileReader(file)) {
+            Set<Character> set = new TreeSet<>();
+            int readValue;
+            while ((readValue = reader.read()) != -1) {
+                if (Character.isLetter(readValue)) {
+                    set.add((char) Character.toLowerCase(readValue));
+                }
+            }
+            StringBuilder builder = new StringBuilder();
+            Iterator<Character> iterator = set.iterator();
+            for (int i = 0; i < SIZE_OF_OUTPUT_STRING; i++) {
+                if (iterator.hasNext()) {
+                    builder.append(iterator.next());
+                }
+            }
+            return builder.toString();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return "";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
