66aad58de952cda34d615f59ba8002a850e21f78
Kokhanevich <nike.k1995@gmail.com>
2019-09-03 20:34:52 +0300

    added solution for HW10-TreeSet

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +23,31 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
+
+    public static final int SIZE = 5;
+
+    public String getUniqueCharacters(String fileName) throws IOException  {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            Set<Character> charSet = new TreeSet<>();
+            while (bufferedReader.ready()) {
+                char [] letters = bufferedReader.readLine().replaceAll("[^a-zA-Z]", "")
+                       .toLowerCase().toCharArray();
+                for (int i = 0; i < letters.length; i++) {
+                    charSet.add(letters[i]);
+                }
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Character character : charSet) {
+                stringBuilder.append(character);
+            }
+            String value = stringBuilder.toString();
+            if (value.length() > SIZE) {
+                return value.substring(0, SIZE);
+            }
+            return value;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
