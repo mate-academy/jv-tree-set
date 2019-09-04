84389a1a053df7d719fe58e08f427a0849f46476
Rosinskyi <sergey22061993@gmail.com>
2019-09-04 18:34:06 +0300

    added solution for TreeSetCharacters

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +23,33 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public static String getUniqueCharacters(String fileName) {
+        int outputLength = 5;
+        String line;
+        String[] parsed;
+        Set<String> treeSet = new TreeSet<String>();
+        StringBuffer stringBuilder = new StringBuffer();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            stringBuilder.append(bufferedReader.readLine());
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+            }
+            line = stringBuilder.toString().strip().toLowerCase().replaceAll("[^A-Za-z]", "");
+            parsed = new String[line.length()];
+
+            for (int i = 0; i < line.length(); i++) {
+                parsed[i] = Character.toString(line.charAt(i));
+            }
+            for (String c : parsed) {
+                treeSet.add(c);
+            }
+            if (treeSet.size() >= outputLength) {
+                return treeSet.toString().replaceAll("[^A-Za-z]", "").substring(0, outputLength);
+            }
+            return treeSet.toString().replaceAll("[^A-Za-z]", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return treeSet.toString();
     }
 }
