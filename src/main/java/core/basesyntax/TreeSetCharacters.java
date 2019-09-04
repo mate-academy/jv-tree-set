32a33a8b6bc1e6d11a8d83f76386d3232cdef5ca
Max <skaterweather41@gmail.com>
2019-09-04 12:47:20 +0200

    added optiaml&fast solution to task 1 TreeSet

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,10 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +22,31 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    private static final int LETTERS_AMOUNT = 5;
+
+    public String getUniqueCharacters(String fileName) throws IOException {
+        TreeSet<Character> lettersFromFile = new TreeSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int input = reader.read();
+            while (input != -1) {
+                if (input >= 'A' && input <= 'Z') {
+                    input += 32;
+                }
+                if (input >= 'a' && input <= 'z') {
+                    lettersFromFile.add((char) input);
+                }
+                input = reader.read();
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        int border = LETTERS_AMOUNT;
+        for (char letter : lettersFromFile) {
+            result.append(letter);
+            if (border == 1) {
+                break;
+            }
+            border--;
+        }
+        return new String(result);
     }
 }
