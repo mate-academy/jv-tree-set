bdcfd018c9ce7c009cd08a57f5fa6722355f443e
roman_minevich <283389dv3qsh>
2019-09-04 14:27:58 +0300

    Added solution

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,23 +1,38 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
- * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
- * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
- *
- * <p>Метод повинен відсортувати всі букви по алфавіту і повернути строку з 5 різних букв без
+ * Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
+ * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.
+ * Метод повинен відсортувати всі букви по алфавіту і повернути строку з 5 різних букв без
  * розділових знаків.
  * Якщо у файлі міститься менше п'яти різних букв, то потрібно повернути їх усі.
  * Можливий випадок, коли переданого файлу не існує, в такому разі потрібно викинути помилку
- * про відсутність файлу.</p>
- *
- * <p>Приклад 1: ur-BvT?^ ra w; p
- * Результат 1: abprt</p>
- *
- * <p>Приклад 2: A _f*c a?F
- * Результат 2: acf</p>
+ * про відсутність файлу.
+ * Приклад 1: ur-BvT?^ ra w; p
+ * Результат 1: abprt
+ * Приклад 2: A _f*c a?F
+ * Результат 2: acf
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        final int RESULT_LENGTH = 5;
+        Set<String> characters = new TreeSet<>();
+        try (FileReader reader = new FileReader(new File(fileName))) {
+            int character;
+            while ((character = reader.read()) != -1) {
+                if (Character.isLetter(character)) {
+                    character = Character.toLowerCase(character);
+                    characters.add(Character.toString(character));
+                }
+            }
+        }
+        String line = String.join("", characters);
+        return (line.length() > RESULT_LENGTH) ? line.substring(0, 5) : line;
     }
 }
