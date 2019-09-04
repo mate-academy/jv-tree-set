4f3451d1ed94a89a13f6a12e9e5518b77d9f3b40
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-09-04 16:35:47 +0300

    added solving a task using a TreeSet

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,14 +1,19 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
  *
  * <p>Метод повинен відсортувати всі букви по алфавіту і повернути строку з 5 різних букв без
- * розділових знаків.
- * Якщо у файлі міститься менше п'яти різних букв, то потрібно повернути їх усі.
- * Можливий випадок, коли переданого файлу не існує, в такому разі потрібно викинути помилку
- * про відсутність файлу.</p>
+ * розділових знаків. Якщо у файлі міститься менше п'яти різних букв, то потрібно повернути їх усі.
+ * Можливий випадок, коли переданого файлу не існує, в такому разі потрібно викинути помилку про
+ * відсутність файлу.</p>
  *
  * <p>Приклад 1: ur-BvT?^ ra w; p
  * Результат 1: abprt</p>
@@ -17,7 +22,32 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+
+    private static final int NUMBER_OF_RETURNED_VALUES = 5;
+
+    public String getUniqueCharacters(String fileName) throws IOException {
+        StringBuilder result = new StringBuilder();
+        try (FileReader reader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(reader)) {
+            StringBuilder textFromFile = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                textFromFile.append(line);
+            }
+            String lettersString = textFromFile.toString().toLowerCase()
+                    .replaceAll("[^A-Za-z]", "");
+            TreeSet<Character> letters = new TreeSet<>();
+            for (char letter : lettersString.toCharArray()) {
+                letters.add(letter);
+            }
+
+            Iterator<Character> iterator = letters.iterator();
+            for (int i = 0; i < NUMBER_OF_RETURNED_VALUES; i++) {
+                if (iterator.hasNext()) {
+                    result.append(iterator.next());
+                }
+            }
+        }
+        return result.toString();
     }
 }
