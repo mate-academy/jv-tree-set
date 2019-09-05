83704e9d08429c564939b32be0b3c480fdae85d5
lenovo <suleimanovayev@gmail.com>
2019-09-05 12:15:39 +0300

    add treeSet

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,23 +1,44 @@
 package core.basesyntax;
 
-/**
- * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
- * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
- *
- * <p>Метод повинен відсортувати всі букви по алфавіту і повернути строку з 5 різних букв без
- * розділових знаків.
- * Якщо у файлі міститься менше п'яти різних букв, то потрібно повернути їх усі.
- * Можливий випадок, коли переданого файлу не існує, в такому разі потрібно викинути помилку
- * про відсутність файлу.</p>
- *
- * <p>Приклад 1: ur-BvT?^ ra w; p
- * Результат 1: abprt</p>
- *
- * <p>Приклад 2: A _f*c a?F
- * Результат 2: acf</p>
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+/**	/**
+ * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.	 * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
+ * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>	 * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
+ @@ -17,7 +24,28 @@
+  * Результат 2: acf</p>	 * Результат 2: acf</p>
  */
+
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    private static final int LENGTH_FIFTH = 5;
+
+    public String getUniqueCharacters(String fileName) throws IOException {
+        String stringChars;
+        String result = "";
+        Set<Character> set = new TreeSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int countOfSmbl = reader.read();
+            do {
+                set.add((char) countOfSmbl);
+                countOfSmbl = reader.read();
+            } while (countOfSmbl != -1);
+            stringChars = set.toString().replaceAll("[^a-zA-Z]", "").toLowerCase();
+            for (int i = 0; i < stringChars.length(); i++) {
+                if (!result.contains(stringChars.substring(i, i + 1))) {
+                    result += stringChars.substring(i, i + 1);
+                }
+            }
+        }
+        char[] chars = result.toCharArray();
+        Arrays.sort(chars);
+        return chars.length > LENGTH_FIFTH ? String.valueOf(chars)
+                .substring(0, LENGTH_FIFTH) : String.valueOf(chars);
     }
 }
