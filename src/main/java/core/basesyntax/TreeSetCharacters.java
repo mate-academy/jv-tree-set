01338d0f9f2e0eff20c3dcfcb854b173087176b4
Stanislav Karev <karsv.dvz@gmail.com>
2019-12-17 13:25:26 +0200

    add solution hw101

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +24,33 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        String getString = getStringFromFile(fileName).toLowerCase();
+
+        Set<Character> set = new TreeSet();
+        for (int i = 0; i < getString.length(); i++) {
+            if (Character.isLetter(getString.charAt(i))) {
+                set.add(getString.charAt(i));
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 5; i++) {
+            if (i < set.toArray().length) {
+                stringBuilder.append(set.toArray()[i]);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getStringFromFile(String file) throws IOException {
+        checkFile(file);
+        return Files.readString(Paths.get(file));
+    }
+
+    private void checkFile(String file) throws IOException {
+        File getFile = new File(file);
+        if (!getFile.exists()) {
+            throw new IOException();
+        }
     }
 }
