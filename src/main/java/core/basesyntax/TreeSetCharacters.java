e37dad151b17f79b847323e2fc1792195a7903e6
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2019-12-19 14:37:29 +0200

    Added solution for hw 10.1

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +24,29 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws FileNotFoundException {
+        String result = "";
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            Set<Character> chars = new TreeSet<>();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String cleanLine = line.replaceAll("[^A-Za-z]", "");
+                for (Character c: cleanLine.toLowerCase().toCharArray()) {
+                    chars.add(c);
+                }
+            }
+
+            StringBuilder charsSet = new StringBuilder();
+            for (Character c: chars) {
+                charsSet.append(c);
+            }
+            int n = Math.min(chars.size(), 5);
+            result = charsSet.toString().substring(0, n);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new FileNotFoundException("There is no such file");
+        }
+        return result;
     }
 }
