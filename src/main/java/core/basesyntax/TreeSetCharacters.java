6a26650139bbb9766486a24bf2f6cbb766a34b26
AlinaPodoliaka <57722396+AlinaPodoliaka@users.noreply.github.com>
2019-12-20 12:02:05 +0200

    added hw-10-1-solution

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Comparator;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +24,53 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+
+    public String getUniqueCharacters(String fileName) throws FileNotFoundException {
+
+        TreeSet<Character> set = new TreeSet<Character>(new Comparator<Character>() {
+            @Override
+            public int compare(Character o1, Character o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        if (fileName != null) {
+            try
+                    (FileReader fileReader = new FileReader(fileName);
+                     BufferedReader reader = new BufferedReader(fileReader);) {
+                while (reader.ready()) {
+                    sb.append(reader.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else {
+            throw new
+                    FileNotFoundException("Файлу не існує");
+        }
+
+        String str = sb.toString().toLowerCase();
+        char[] array = str.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            if (Character.isLetter(array[i])) {
+                set.add(array[i]);
+            }
+        }
+        String res = "";
+        if (set.isEmpty()) {
+            return res;
+        }
+        if (set.size() > 5) {
+            for (int i = 0; i < 5; i++) {
+                res += set.pollFirst();
+            }
+        }
+        if (set.size() < 5) {
+            for (int i = 0; i < set.size() + i; i++) {
+                res += set.pollFirst();
+            }
+        }
+        return res;
     }
 }
