d8f347733aac410bd9bd63dc105c9ddbb1050fff
RUSLAN SIMAKOV <UA667766706@GMAIL.COM>
2019-12-22 12:24:27 +0200

    have a nice day)

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,13 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +25,28 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        List<String> lines;
+        try {
+            lines = Files.readAllLines(Paths.get(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        String clearedData = lines.toString().toLowerCase().replaceAll("[^a-zA-Z]+", "");
+        SortedSet<Character> uniqueCharacters = new TreeSet();
+        for (int i = 0; i < clearedData.length(); i++) {
+            uniqueCharacters.add(clearedData.charAt(i));
+        }
+        StringBuilder returnedData = new StringBuilder();
+        Iterator iterator = uniqueCharacters.iterator();
+
+        while (iterator.hasNext()) {
+            returnedData.append(iterator.next().toString());
+            if (returnedData.length() == 5) {
+                return returnedData.toString();
+            }
+        }
+        return returnedData.toString();
     }
 }
