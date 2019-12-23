ceab84ffbbdc3c03c14fa1f758d08c92d6d69c0f
Vadim Chaika <proxik777@gmail.com>
2019-12-23 02:21:48 +0200

    added a solution for TreeSet hw

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +23,25 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        if (Files.exists(Paths.get(fileName))) {
+            Set<Character> charSet = new TreeSet<>();
+            char [] charArray = Files.readString(Paths.get(fileName))
+                    .replaceAll("[^a-zA-Z]", "").toLowerCase().toCharArray();
+            for (char i : charArray) {
+                charSet.add(i);
+            }
+            StringBuilder res = new StringBuilder();
+            for (char i : charSet) {
+                res.append(i);
+                if (res.length() >= 5) {
+                    break;
+                }
+            }
+            return res.toString().trim();
+        } else {
+            throw new IOException("File doesn't exist.");
+        }
+
     }
 }
