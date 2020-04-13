f7719839ab403d93fcdc501be834f5f5f31f22c4
vladyslav <vlad.bes0711@gmail.com>
2020-04-13 18:49:49 +0300

    fixed code style issues

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -37,7 +37,7 @@ public class TreeSetCharacters {
         StringBuilder result = new StringBuilder();
         for (Character elem : treeSet) {
             result.append(elem);
-            if (result.length() > 4){
+            if (result.length() >= 5) {
                 break;
             }
         }
