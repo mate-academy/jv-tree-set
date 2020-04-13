d95c729503130a0f65f7d09338006d99867ce6f7
Sumned <vladar91@gmail.com>
2020-04-13 22:28:15 +0300

    exception removed, nio removed, io added, loop updated

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,31 +1,31 @@
 package core.basesyntax;
 
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) throws FileNotFoundException {
+    public String getUniqueCharacters(String fileName) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
         StringBuilder chapter = new StringBuilder();
         TreeSet<Character> treeSet = new TreeSet<>();
-        try {
-            Files.lines(Paths.get(fileName)).forEach(stringBuilder::append);
-        } catch (IOException e) {
-            throw new FileNotFoundException(e.toString());
+        FileReader reader = new FileReader(fileName);
+        while (reader.ready()) {
+            stringBuilder.append((char) reader.read());
         }
+        reader.close();
         char[] chars = stringBuilder.toString().toCharArray();
         for (Character i : chars) {
             if (Character.isLetter(i)) {
                 treeSet.add(Character.toLowerCase(i));
             }
         }
-        int x = Math.min(treeSet.size(), 5);
-        for (int j = 0; j < x; j++) {
-            chapter.append(treeSet.first());
-            treeSet.remove(treeSet.first());
+        Iterator<Character> iterator = treeSet.iterator();
+        int x = 5;
+        while (iterator.hasNext() && x > 0) {
+            chapter.append(iterator.next());
+            x--;
         }
         return chapter.toString();
     }
