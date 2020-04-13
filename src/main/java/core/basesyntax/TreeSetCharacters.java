a484477bad878576e6b95784d278371d53a7769e
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-13 13:53:11 +0300

    Added solution.

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +24,25 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName));) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line.toLowerCase());
+            }
+        }
+        Set<Character> set = new TreeSet<>();
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (Character.isLetter(stringBuilder.charAt(i))) {
+                set.add(stringBuilder.charAt(i));
+            }
+        }
+        Iterator<Character> iterator = set.iterator();
+        String result = "";
+        while (iterator.hasNext()) {
+            result += iterator.next();
+        }
+        return result.length() < 6 ? result : result.substring(0, 5);
     }
 }
