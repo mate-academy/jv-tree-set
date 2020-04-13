d0477ce29d31e65bb2593dc33bc9ba78a3ed497e
Ludchenko <skillet367@icloud.com>
2020-04-13 22:02:48 +0300

    fixed mvn errors

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,9 @@
 package core.basesyntax;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +21,28 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        TreeSet<Character> treeSet = new TreeSet<>();
+        FileReader fileReader = new FileReader(fileName);
+
+        int data = fileReader.read();
+
+        while (data != -1) {
+            if (Character.isAlphabetic(data)) {
+                treeSet.add((char) Character.toLowerCase(data));
+            }
+            data = fileReader.read();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (Character c : treeSet) {
+            if (i == 5) {
+                break;
+            }
+            sb.append(c);
+            i++;
+        }
+        return sb.toString();
     }
 }
