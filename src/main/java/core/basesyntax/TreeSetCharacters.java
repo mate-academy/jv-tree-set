f615a3f7fb3c9ef0a60a997b79c389b6e0a1ca88
Максим Матуляк <maksymmatuliak@gmail.com>
2020-04-13 15:42:50 +0300

    deleted not used imports

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,13 +1,9 @@
 package core.basesyntax;
 
-import javax.imageio.IIOException;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
