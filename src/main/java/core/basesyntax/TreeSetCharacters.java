954e1b51905f04c483231fe8c571a3715964df4a
SpringWay <44027294+SpringWay@users.noreply.github.com>
2020-04-13 17:48:00 +0300

    Solution to hw-10.1

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +23,27 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws FileNotFoundException {
+        TreeSet<Character> treeSet = new TreeSet<>();
+        try (FileReader ourFile = new FileReader(fileName)) {
+            int data = ourFile.read();
+            while (data != -1) {
+                if (Character.isAlphabetic(data)) {
+                    treeSet.add((char) Character.toLowerCase(data));
+                }
+                data = ourFile.read();
+            }
+
+        } catch (IOException e) {
+            throw new FileNotFoundException("Такого файлу не існує!");
+        }
+        Iterator<Character> iterator = treeSet.iterator();
+        StringBuilder builder = new StringBuilder();
+        int i = 0;
+        while (iterator.hasNext() && i < 5) {
+            builder.append(iterator.next());
+            i++;
+        }
+        return builder.toString();
     }
 }
