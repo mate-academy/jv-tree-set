d268680a6b7890a0c1dcc586b8d038bcc419d252
Marina <maryna.voloshyna.11@gmail.com>
2020-08-17 22:22:09 +0300

    solution 1

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -4,52 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
-/**
- * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
- * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
- *
- * <p>Метод повинен відсортувати всі букви по алфавіту і повернути строку з 5 різних букв без
- * розділових знаків.
- * Якщо у файлі міститься менше п'яти різних букв, то потрібно повернути їх усі.
- * Можливий випадок, коли переданого файлу не існує, в такому разі потрібно викинути помилку
- * про відсутність файлу.</p>
- *
- * <p>Приклад 1: ur-BvT?^ ra w; p
- * Результат 1: abprt</p>
- *
- * <p>Приклад 2: A _f*c a?F
- * Результат 2: acf</p>
- */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) throws FileNotFoundException {
-        StringBuilder sb = new StringBuilder();
-        Set<Character> set = new TreeSet<>();
+    public String getUniqueCharacters(String fileName) throws IOException {
+        StringBuilder characters = new StringBuilder();
+        Set<Character> setOfCharacters = new TreeSet<>();
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
-                while ((line = br.readLine()) != null) {
-                    char[] chars = line.toLowerCase().replaceAll("[^a-z]", "").toCharArray();
-                    for (int i = 0; i < chars.length; i++) {
-                        set.add(chars[i]);
-                    }
+            while ((line = br.readLine()) != null) {
+                char[] chars = line.toLowerCase().replaceAll("[^a-z]", "").toCharArray();
+                for (int i = 0; i < chars.length; i++) {
+                    setOfCharacters.add(chars[i]);
                 }
+            }
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException("File not found");
-        } catch (IOException e) {
-            e.printStackTrace();
         }
         int temp = 0;
-        for (Character element : set) {
+        for (Character element : setOfCharacters) {
             if (temp == 5) {
-                return sb.toString();
+                return characters.toString();
             }
-            sb.append(element);
+            characters.append(element);
             temp++;
         }
-        return sb.toString();
+        return characters.toString();
     }
 }
