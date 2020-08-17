6bd44455c92ac3cff537d95dc82150e84dfe95a3
jlightgreen <ShvabovichJulia@gmail.com>
2020-08-17 17:58:53 +0300

    hw-16.1

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,11 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +23,24 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        Set<Character> letters = new TreeSet<>();
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        while (reader.ready()) {
+            int character = reader.read();
+            if (Character.isLetter(character)) {
+                letters.add((char) Character.toLowerCase(character));
+            }
+        }
+        reader.close();
+
+        StringBuilder result = new StringBuilder();
+        for (Character c : letters) {
+            result.append(c);
+        }
+        if (result.length() < 5) {
+            return result.toString();
+        }
+        return result.toString().substring(0, 5);
     }
 }
