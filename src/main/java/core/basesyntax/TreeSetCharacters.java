61f91075ceab4de555a9ea9c7f854d19b7fe05dd
aleksandr.hrankin <aleksandr.hrankin@gmail.com>
2020-08-17 21:31:44 +0300

    Added methods: readFile, getCharsFromString, setToString. Added buisness logic for method getUniqueCharacters

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,5 +1,12 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
  * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
@@ -17,7 +24,42 @@ package core.basesyntax;
  * Результат 2: acf</p>
  */
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        String data = readFile(fileName);
+        data = data.replaceAll("(\\W|\\d)", "").toLowerCase();
+        Set<Character> chars = getCharsFromString(data);
+        return setToString(chars);
+    }
+
+    private String setToString(Set<Character> chars) {
+        StringBuilder result = new StringBuilder();
+        int index = 0;
+        for (Character letter : chars) {
+            result.append(letter);
+            index++;
+            if (index == 5) {
+                break;
+            }
+        }
+        return result.toString();
+    }
+
+    private Set<Character> getCharsFromString(String data) {
+        Set<Character> chars = new TreeSet<>();
+        for (int i = 0; i < data.length(); i++) {
+            chars.add(data.charAt(i));
+        }
+        return chars;
+    }
+
+    private String readFile(String filename) throws IOException {
+        StringBuilder lines = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.append(line);
+            }
+        }
+        return lines.toString();
     }
 }
