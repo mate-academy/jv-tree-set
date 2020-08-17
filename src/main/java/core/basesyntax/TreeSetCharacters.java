d99e672bb0bdeb3591c8eb21a86ec1a824fedc49
Dm_Grt <grytsishin@gmail.com>
2020-08-17 17:02:57 +0300

    getUniqueChars implementation added

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,23 +1,30 @@
 package core.basesyntax;
 
-/**
- * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
- * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
- *
- * <p>Метод повинен відсортувати всі букви по алфавіту і повернути строку з 5 різних букв без
- * розділових знаків.
- * Якщо у файлі міститься менше п'яти різних букв, то потрібно повернути їх усі.
- * Можливий випадок, коли переданого файлу не існує, в такому разі потрібно викинути помилку
- * про відсутність файлу.</p>
- *
- * <p>Приклад 1: ur-BvT?^ ra w; p
- * Результат 1: abprt</p>
- *
- * <p>Приклад 2: A _f*c a?F
- * Результат 2: acf</p>
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.TreeSet;
+
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    private static final int MAX_NUMBER = 5;
+
+    public String getUniqueCharacters(String fileName) throws IOException {
+        TreeSet<Character> letters = new TreeSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            int character = reader.read();
+            while (character != -1) {
+                if (Character.isLetter(character)) {
+                    letters.add((char) Character.toLowerCase(character));
+                }
+                character = reader.read();
+            }
+        }
+        StringBuilder response = new StringBuilder();
+        Iterator<Character> iterator = letters.iterator();
+        while (iterator.hasNext() && response.length() < MAX_NUMBER) {
+            response.append(iterator.next());
+        }
+        return response.toString();
     }
 }
