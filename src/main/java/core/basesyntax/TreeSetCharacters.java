7c29fa2676f1b10833d14bbc6231c914bbc4349b
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-08-18 01:38:12 +0300

    solution added

--- src/main/java/core/basesyntax/TreeSetCharacters.java
+++ src/main/java/core/basesyntax/TreeSetCharacters.java
@@ -1,23 +1,24 @@
 package core.basesyntax;
 
-/**
- * <p>Реалізуйте метод `getUniqueCharacters(String fileName)` який приймає як параметр назву файлу.
- * Для цього використовуйте TreeSet. Файл містить букви латинського алфавіту і розділові знаки.</p>
- *
- * <p>Метод повинен відсортувати всі букви по алфавіту і повернути строку з 5 різних букв без
- * розділових знаків.
- * Якщо у файлі міститься менше п'яти різних букв, то потрібно повернути їх усі.
- * Можливий випадок, коли переданого файлу не існує, в такому разі потрібно викинути помилку
- * про відсутність файлу.</p>
- *
- * <p>Приклад 1: ur-BvT?^ ra w; p
- * Результат 1: abprt</p>
- *
- * <p>Приклад 2: A _f*c a?F
- * Результат 2: acf</p>
- */
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
+
 public class TreeSetCharacters {
-    public String getUniqueCharacters(String fileName) {
-        return null;
+    public String getUniqueCharacters(String fileName) throws IOException {
+        StringBuilder resultString = new StringBuilder();
+        TreeSet<Character> charSet = new TreeSet<Character>();
+        try (FileReader fileReader = new FileReader(fileName)) {
+            int symbol;
+            while ((symbol = fileReader.read()) != -1) {
+                charSet.add((char) Character.toLowerCase(symbol));
+            }
+            for (Character ch: charSet) {
+                if (Character.isLetter(ch) && (resultString.length() < 5)) {
+                    resultString.append(ch);
+                }
+            }
+        }
+        return resultString.toString().toLowerCase();
     }
 }
